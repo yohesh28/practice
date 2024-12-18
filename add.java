6f40d90a9f0324f61aa620cc40fd35bfbfbc5b9c
Yoheshwaran kcube <yoheshwaran@kcubeconsulting.com>
2024-12-18 12:08:08 +0530

    add function added

---
+++ add.java
@@ -0,0 +1,8 @@
+public class add {
+    public static void main(String[] args) {
+        int number1 = 1;
+        int number2 = 2;
+        int sum = number1 + number2;
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
